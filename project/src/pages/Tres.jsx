00f7ad3f6703301e86b22b61602e59c1b7ce2bc0
guilherme augusto <guih107@gmail.com>
2024-03-03 09:18:32 -0300

    botoes home

---
+++ project/src/pages/Tres.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Tres() {
+  return (
+    <>
+      <h1>Tres</h1>
+    </>
+  );
+}
+
+export default Tres;
--- project/src/pages/home.jsx
+++ project/src/pages/home.jsx
@@ -5,12 +5,25 @@ function Home() {
   return (
    <>
     <Header />
-    <div>
+    <img src="https://pedagogiaaopedaletra.com/wp-content/uploads/2011/09/recreacaoejogos.jpg" alt="Criança brincando" />
+    <nav>
       <label htmlFor="">
-        <button>Infantil 3 á 6 anos</button>
+        <button
+          onClick={() => {
+            console.log("Clicou");
+          }}
+        >
+          Infantil 3 á 6 anos
+          </button>
       </label>
       <label htmlFor="">
-        <button>Infantil 7 á 12 anos</button>
+        <button
+          onClick={() => {
+            console.log("Clicou");
+          }}
+        >
+          Infantil 7 á 12 anos
+        </button>
       </label>
       <label htmlFor="">
         <button>Adolescentes</button>
@@ -18,7 +31,7 @@ function Home() {
       <label htmlFor="">
         <button>Adultos</button>
       </label>
-    </div>
+    </nav>
   </>
   );
 }
